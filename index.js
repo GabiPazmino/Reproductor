270580422ca3a417e0ab32ca444b162210a24b03
GabiPazmino <gabi.pazmino.armijos@gmail.com>
2024-02-23 00:08:16 -0500

    login

---
+++ index.js
@@ -0,0 +1,30 @@
+const userInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const loginBoton = document.getElementById("loginbtn");
+
+
+const users = [
+    {username: "gabi",
+    password: "gabi"},
+    {username: "simone",
+    password: "simone"}
+]
+
+
+loginBoton.addEventListener("click", (event) =>{
+    event.preventDefault();
+    
+    let userValue = userInput.value;
+    let passwordValue = passwordInput.value;
+
+    let logged=false;
+    users.forEach(user => {
+        if(user.username == userValue && user.passwordValue == passwordValue){logged =true;}
+    });
+
+    if(logged){
+        alert("Logged in");
+    }else{
+        alert("Wrong credentials");
+    }
+})
